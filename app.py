1c44c977aca161df5c8854aba95ed8418bfb1634
HANIF134 <nokhanif@gmail.com>
2025-01-13 18:01:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,15 @@ from streamlit_extras.badges import badge
 # Load Model SVM
 @st.cache_resource
 def load_model():
+
     with open("model/svm_sentiment_model.pkl", "rb") as model_file:
+
         model = pickle.load(model_file)
+
     with open("model/tfidf_vectorizer.pkl", "rb") as vec_file:
+
         vectorizer = pickle.load(vec_file)
+
     return model, vectorizer
 
 # Hasil load model diubah ke dalam bentuk Vector
